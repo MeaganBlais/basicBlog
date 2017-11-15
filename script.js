677568b6c16974fda5f4ac744446c7f1edcdecb6
Meagan Blais <meaganblais@gmail.com>
2017-11-15 00:19:24 -0500

    adding like button

---
+++ script.js
@@ -0,0 +1,14 @@
+// $(element).on(event-type, thing-to-be-done);
+// $(selector).event-listener(event-type, action);
+
+$("button").on("click", function() {
+  alert("clicked")
+});
+
+// $("button").on("hover", function() {
+//   alert("hovering")
+// });
+
+// $("button").on("scroll", function() {
+//   alert("clicked")
+// });
